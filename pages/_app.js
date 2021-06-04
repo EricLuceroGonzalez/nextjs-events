a9c60e4edc1ca2bf42fcb16f223b4256d0492454
Eric Lucero Gonzalez <ericlucerogonzalez@gmail.com>
2021-06-04 00:47:28 -0500

    setting headers to app and document

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,18 @@
 import "../styles/globals.css";
 import LayoutComponent from "../components/layout/layout";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <LayoutComponent>
+      <Head>
+        <title>My Events App</title>
+        <meta
+          name="description"
+          content="Find a lot of events."
+        />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Component {...pageProps} />
     </LayoutComponent>
   );
